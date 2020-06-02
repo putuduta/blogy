e16a4d6c90f0b55cc582346d5ca3d360af5780a6
putuduta <putuduta@gmail.com>
2020-06-02 11:20:18 +0700

    Complete Blog Website

--- app.js
+++ app.js
@@ -5,12 +5,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 //starting content
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts = [];
+
+const postSchema = {
+
+  title: String,
+
+  content: String
+
+};
+
+const Post = mongoose.model("Post", postSchema);
 
 const app = express();
 
@@ -27,11 +42,17 @@ app.use(express.static("public"));
 //get the home
 app.get('/', (req, res) => {
 
-  res.render('index', {
-    title: homeStartingContent,
-    posts: posts
-  });
+  Post.find({}, (err, posts) => {
 
+    res.render("index", {
+
+      startingContent: homeStartingContent,
+
+      posts: posts
+
+    });
+
+  });
 });
 
 //get the contact
@@ -63,28 +84,41 @@ app.get('/compose', (req, res) => {
 //post the content
 app.post('/compose', (req, res) => {
 
-  let post = {
-    title: req.body.newTitle,
-    content: req.body.postTitle
-  };
-  posts.push(post);
-  res.redirect('/');
+  const post = new Post({
 
+    title: req.body.newTitle,
+
+    content: req.body.postTitle
+
+  });
+  post.save((err) => {
+
+    if (!err) {
+
+      res.redirect("/");
+
+    }
+
+  });
 });
 
 //go to posts page
-app.get('/posts/:title', (req, res) => {
+app.get('/posts/:postId', (req, res) => {
   //truncate the title
-  let reqTitle = _.lowerCase(req.params.title);
+  const requestedPostId = req.params.postId;
 
-  posts.forEach(element => {
-    let theTitle = _.lowerCase(element.title);
-    if (reqTitle === theTitle) {
-      res.render('post', {
-        pTitle: element.title,
-        pBody: element.content
-      });
-    }
+  Post.findOne({
+    _id: requestedPostId
+  }, (err, post) => {
+
+    res.render("post", {
+
+      title: post.title,
+
+      content: post.content
+
+    });
+
   });
 
 });
